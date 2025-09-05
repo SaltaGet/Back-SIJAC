139ff0ed2753dbdcab80127df1806eb5fd0ec517
DanielChachagua <danielmchachagua@gmail.com>
2025-09-04 21:22:34 -0300

    update cors

--- src/__init__.py
+++ src/__init__.py
@@ -59,7 +59,7 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=False,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allow_headers=["http://localhost"],
+    allow_headers=["Authorization", "Content-Type", "Accept"],
 )
 
 app.add_middleware(TimingMiddleware)
